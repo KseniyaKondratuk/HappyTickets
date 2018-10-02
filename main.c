0498673884c9bd75b43e17aca827b67d56aef46a
KseniyaKondratuk <xenia.kondratuk@gmail.com>
2018-10-03 00:41:45 +0300

    add consts

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+const int MIN_SIX_VALUE_NUMBER = 100000;
+const int MAX_SIX_VALUE_NUMBER = 1000000;
+
 int getSumOfNumbers(int number){
     return number % 10 + number / 100 + number / 10 % 10;
 }
 
 int main() {
     int countOfHappyTickets = 0, suffix, prefix;
-    for (int i = 100000; i < 1000000; ++i) {
+    for (int i = MIN_SIX_VALUE_NUMBER; i < MAX_SIX_VALUE_NUMBER; ++i) {
         prefix = i / 1000;
         suffix = i % 1000;
         if (getSumOfNumbers(prefix) == getSumOfNumbers(suffix)) {
